20658b5418cc7c242520595a7047f95d66c3e641
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-28 18:54:10 +0900

    update doc (tag: master)

--- master/implementors/core/ops/trait.Deref.js
+++ master/implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["gurobi"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.Context.html' title='gurobi::Context'>Context</a>&lt;'a&gt;",];
+implementors["gurobi"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.Callback.html' title='gurobi::Callback'>Callback</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
