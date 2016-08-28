f153625333737a8d3d3e17680afdadad9c414e76
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-29 04:46:28 +0900

    WIP: add example: sensitivity

---
+++ examples/sensitivity.rs
@@ -0,0 +1,80 @@
+// Copyright (c) 2016 Yusuke Sasaki
+//
+// This software is released under the MIT License.
+// See http://opensource.org/licenses/mit-license.php or <LICENSE>.
+
+extern crate gurobi;
+use gurobi::*;
+use std::env::args;
+
+fn main() {
+  let mut env = Env::new("").unwrap();
+  let mut model = env.read_model(args().nth(1).as_ref().unwrap()).unwrap();
+  assert!(model.get(attr::IsMIP).unwrap() == 1, "Model is not a MIP");
+
+  model.optimize().unwrap();
+
+  let status = model.status().unwrap();
+  assert!(status == Status::Optimal,
+          "Optimization ended with status {:?}",
+          status);
+
+  // store the optimal solution.
+  let orig_obj = model.get(attr::ObjVal).unwrap();
+  let orig_sol: Vec<_> = model.get_vars().map(|v| v.get(&model, attr::X).unwrap()).collect();
+
+  // disable solver output for subsequent solvers.
+  env.set(param::OutputFlag, 0).unwrap();
+
+  // iterate through unfixed, binary variables in model
+  let mut vars: Vec<_> = model.get_vars().cloned().collect();
+  for (idx, (&mut v, &orig_x)) in vars.iter_mut().zip(orig_sol.iter()).enumerate() {
+    let mut lb = v.get(&model, attr::LB).unwrap();
+    let mut ub = v.get(&model, attr::UB).unwrap();
+    let vtype = v.get(&model, attr::VType).unwrap();
+    let vname = v.get(&model, attr::VarName).unwrap();
+
+    use std::mem::transmute;
+    if lb == 0.0 && ub == 1.0 &&
+       (vtype == unsafe { transmute('B' as i8) } || vtype == unsafe { transmute('I' as i8) }) {
+      // set variable to 1 - x, where x is its value in optimal solution
+      // (it means that `negate` value of the binary variable)
+      let mut start = 0.0;
+      if orig_x < 0.5 {
+        lb = 1.0;
+        start = 1.0;
+      } else {
+        ub = 0.0;
+        start = 0.0;
+      }
+      v.set(&mut model, attr::LB, lb).unwrap();
+      v.set(&mut model, attr::UB, ub).unwrap();
+      v.set(&mut model, attr::Start, start).unwrap();
+
+      // update MIP start for the other variables.
+      for (idxx, (&mut vv, &orig_xx)) in vars.iter_mut().zip(orig_sol.iter()).enumerate() {
+        if idx != idxx {
+          vv.set(&mut model, attr::Start, orig_xx).unwrap();
+        }
+      }
+
+      // solve for new value and capture sensitivity information.
+      model.optimize().unwrap();
+      match model.status().unwrap() {
+        Status::Optimal => {
+          let objval = model.get(attr::ObjVal).unwrap();
+          println!("Objective sensitivity for variable {} is {}",
+                   vname,
+                   objval - orig_obj);
+        }
+        _ => {
+          println!("Objective sensitivity for variable {} is infinite", vname);
+        }
+      }
+
+      // restore the original variable bounds
+      v.set(&mut model, attr::LB, 0.0).unwrap();
+      v.set(&mut model, attr::UB, 1.0).unwrap();
+    }
+  }
+}
