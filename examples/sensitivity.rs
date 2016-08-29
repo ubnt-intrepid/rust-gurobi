dfe76f86459f2dac9b32fc9e5b5340ff7946bd79
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-29 15:49:09 +0900

    revise mutability of Proxy

--- examples/sensitivity.rs
+++ examples/sensitivity.rs
@@ -6,11 +6,13 @@
 extern crate gurobi;
 use gurobi::*;
 use std::env::args;
+use std::mem::transmute;
 
 fn main() {
   let mut env = Env::new("").unwrap();
+  env.set(param::LogToConsole, 0).unwrap();
   let mut model = env.read_model(args().nth(1).as_ref().unwrap()).unwrap();
-  assert!(model.get(attr::IsMIP).unwrap() == 1, "Model is not a MIP");
+  assert!(model.get(attr::IsMIP).unwrap() != 0, "Model is not a MIP");
 
   model.optimize().unwrap();
 
@@ -24,26 +26,28 @@ fn main() {
   let orig_sol: Vec<_> = model.get_vars().map(|v| v.get(&model, attr::X).unwrap()).collect();
 
   // disable solver output for subsequent solvers.
-  env.set(param::OutputFlag, 0).unwrap();
+  // env.set(param::OutputFlag, 0).unwrap();
 
   // iterate through unfixed, binary variables in model
-  let mut vars: Vec<_> = model.get_vars().cloned().collect();
-  for (idx, (&mut v, &orig_x)) in vars.iter_mut().zip(orig_sol.iter()).enumerate() {
-    let mut lb = v.get(&model, attr::LB).unwrap();
-    let mut ub = v.get(&model, attr::UB).unwrap();
+  let vars: Vec<_> = model.get_vars().cloned().collect();
+  for (v, &orig_x) in vars.iter().zip(orig_sol.iter()) {
+    let lb = v.get(&model, attr::LB).unwrap();
+    let ub = v.get(&model, attr::UB).unwrap();
     let vtype = v.get(&model, attr::VType).unwrap();
-    let vname = v.get(&model, attr::VarName).unwrap();
+    let vtype = unsafe { transmute::<_, u8>(vtype) } as char;
 
-    use std::mem::transmute;
-    if lb == 0.0 && ub == 1.0 &&
-       (vtype == unsafe { transmute('B' as i8) } || vtype == unsafe { transmute('I' as i8) }) {
+    if lb == 0.0 && ub == 1.0 && (vtype == 'B' || vtype == 'I') {
+      let vname = v.get(&model, attr::VarName).unwrap();
+
       // set variable to 1 - x, where x is its value in optimal solution
       // (it means that `negate` value of the binary variable)
-      let mut start = 0.0;
+      let (lb, ub, start);
       if orig_x < 0.5 {
         lb = 1.0;
+        ub = 1.0;
         start = 1.0;
       } else {
+        lb = 0.0;
         ub = 0.0;
         start = 0.0;
       }
@@ -52,8 +56,8 @@ fn main() {
       v.set(&mut model, attr::Start, start).unwrap();
 
       // update MIP start for the other variables.
-      for (idxx, (&mut vv, &orig_xx)) in vars.iter_mut().zip(orig_sol.iter()).enumerate() {
-        if idx != idxx {
+      for (vv, &orig_xx) in vars.iter().zip(orig_sol.iter()) {
+        if v != vv {
           vv.set(&mut model, attr::Start, orig_xx).unwrap();
         }
       }
