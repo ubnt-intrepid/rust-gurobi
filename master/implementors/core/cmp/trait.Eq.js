b7777bbccfdd0ded2e9ee532d66cfd9edf4f307a
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-29 17:00:45 +0900

    update doc (tag: master)

--- master/implementors/core/cmp/trait.Eq.js
+++ master/implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["itertools"] = ["impl&lt;A:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>,&nbsp;B:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='itertools/enum.EitherOrBoth.html' title='itertools::EitherOrBoth'>EitherOrBoth</a>&lt;A,&nbsp;B&gt;",];
+implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/cmp/trait.PartialEq.js
+++ master/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='gurobi/enum.Status.html' title='gurobi::Status'>Status</a>",];
+implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='gurobi/enum.Status.html' title='gurobi::Status'>Status</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
