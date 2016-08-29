72a2edb4cc6c52ee538db8922576ecebfe0c87e0
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-30 03:18:58 +0900

    update doc (tag: master)

--- master/implementors/core/ops/trait.Drop.js
+++ master/implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='gurobi/struct.Env.html' title='gurobi::Env'>Env</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='gurobi/struct.Model.html' title='gurobi::Model'>Model</a>&lt;'a&gt;",];
+implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='gurobi/struct.Env.html' title='gurobi::Env'>Env</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='gurobi/struct.Model.html' title='gurobi::Model'>Model</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
