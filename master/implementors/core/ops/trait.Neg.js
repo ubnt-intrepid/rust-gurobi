41b5acd2a190f45b79a0bf1845558c4859f7bcbd
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-01 03:34:37 +0900

    update doc (tag: master)

--- master/implementors/core/ops/trait.Neg.js
+++ master/implementors/core/ops/trait.Neg.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["gurobi_sys"] = [];
+implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for &amp;'a <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
