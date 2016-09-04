447c69cd614cfbc8d1f9fd68e958f0b925f5d192
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-05 01:54:30 +0900

    update doc (tag: v0.3.2)

---
+++ v0.3.2/implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["gurobi"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.Callback.html' title='gurobi::Callback'>Callback</a>&lt;'a&gt;","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.Constr.html' title='gurobi::Constr'>Constr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.QConstr.html' title='gurobi::QConstr'>QConstr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gurobi/struct.SOS.html' title='gurobi::SOS'>SOS</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.3.2/implementors/core/ops/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["gurobi"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='gurobi/struct.Var.html' title='gurobi::Var'>Var</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='gurobi/struct.Constr.html' title='gurobi::Constr'>Constr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='gurobi/struct.QConstr.html' title='gurobi::QConstr'>QConstr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='gurobi/struct.SOS.html' title='gurobi::SOS'>SOS</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
