a187ce1253b6b2f9c3be6d94d22ef48f63cb6691
Dan Dixey <daniel.dixey@itv.com>
2021-01-20 19:09:50 +0000

    Update src/attr.rs
    Co-authored-by: Alice Ryhl <alice@ryhl.io>

--- src/attr.rs
+++ src/attr.rs
@@ -290,7 +290,7 @@ impl AttrArray for StringAttr {
 
     fn to_rawsets(values: &[String]) -> Result<Vec<ffi::c_str>> {
         let mut buf = Vec::with_capacity(values.len());
-        for value in values.iter() {
+        for value in values {
             let value = CString::new(value.as_str())?;
             buf.push(value.as_ptr())
         }
